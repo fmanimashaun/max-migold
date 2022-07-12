8091f7f839fde85817523c66eb9866c3dc89b936
fmanimashaun <fisayo.animashaun@outlook.com>
2022-07-12 01:33:57 +0100

    instroduce the slider section and need javascript

--- in2it-ng/resources/scripts/main.js
+++ in2it-ng/resources/scripts/main.js
@@ -8,7 +8,19 @@ const searchButton = document.querySelector('#search_btn');
 const logoImg = document.querySelector('#logo');
 const menuButton = document.querySelector('#menu_btn');
 const closeButton = document.querySelector('#close_btn');
+const secondaryMenu = document.querySelector('#secondary_nav');
+const sourcesList = document.querySelector('picture.slider_img');
+const rightArrow = document.querySelector('#right_arrow');
+const leftArrow = document.querySelector('#left_arrow');
+let count = 0;
 const bodyWidth = window.innerWidth;
+
+
+//sourcesList.children[0].attributes.srcset.value = 'images/slider/slider_img_1.jpg';
+//sourcesList.children[1].attributes.srcset.value = 'images/slider/slider_img_1.jpg';
+//sourcesList.children[2].attributes.srcset.value = 'images/slider/slider_img_1.jpg';
+//sourcesList.children[3].attributes.srcset.value = 'images/slider/slider_img_1.jpg';
+//sourcesList.children[3].attributes.src.value = 'images/slider/slider_img_1.jpg';
 
 //add event listener for search button
 searchButton.addEventListener('click', function () {
@@ -19,11 +31,13 @@ searchButton.addEventListener('click', function () {
     logoImg.style.flexBasis = '100%';
     nav.style.flexWrap = "wrap";
     logoImg.style.textAlign = "center";
+    if(secondaryMenu.style.display === 'block'){
+        secondaryMenu.style.display = 'none';
+    }
 });
 
 //add event listener for close button
 closeButton.addEventListener('click', function () {
-    console.log('Menu clicked');
     searchBar.style.display = 'none';
     closeButton.style.display = 'none';
     menuButton.style.display = 'inline-block';
@@ -31,6 +45,42 @@ closeButton.addEventListener('click', function () {
     logoImg.style.flexBasis = '23.5%';
     nav.style.flexWrap = "nowrap";
     logoImg.style.textAlign = "left";
+    if(secondaryMenu.style.display === 'block'){
+        secondaryMenu.style.display = 'none';
+    }
+});
+
+//add event listener for menu button 
+menuButton.addEventListener('click', function () {
+    secondaryMenu.style.display = 'block';
+    menuButton.style.display = 'none';
+    closeButton.style.display = 'inline-block';
+});
+
+//add event listener for slider arrows
+
+//right ar
+rightArrow.addEventListener('click', function () {
+    count++;
+    if(count === 4){
+        count = 0;
+        console.log(count);
+        console.log("count reset");
+    }else {
+        console.log(count);
+    }
+});
+
+//left arrow
+leftArrow.addEventListener('click', function () {
+    count--;
+    if(count === -1){
+        count = 3;
+        console.log(count);
+        console.log("count reset");
+    }else {
+        console.log(count);
+    }
 });
 
 
@@ -47,4 +97,3 @@ window.addEventListener("resize", function() {
 });
 
 */
-
